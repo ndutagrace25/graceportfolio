317473622197446c6e88dc3a6a66aaddab621688
ndutagrace25 <ndutagrace25@gmail.com>
2024-05-09 10:12:09 +0300

    finalized on the page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import obscura from "../images/obscura.jpg";
-import environmental from "../images/environmental.jpg";
+import website from "../images/website1.png";
+import dashboad from "../images/dashboad.png";
 import powwater from "../images/powwater.jpg";
 import Fade from "react-reveal/Fade";
+import ts from "../images/Typescript_logo.png";
+import partnership from "../images/partnership.png";
 
 const Projects = ({ projectsDivRef }) => {
   return (
@@ -18,34 +20,37 @@ const Projects = ({ projectsDivRef }) => {
           <Fade delay={520} bottom>
             <div className="d-flex justify-content-between flex-wrap project-card p-3 bg-white rounded shadow-sm">
               <a
-                className="bg-maroon p-3 col-md-6 image-card"
-                href="https://master--brilliant-hotteok-f20887.netlify.app/"
+                className="shadow rounded p-3 col-md-6 image-card"
+                href="https://powwater.com"
                 target="_blank"
                 rel="noreferrer"
               >
                 <img
                   alt="project"
-                  src={obscura}
+                  src={website}
                   className="img-fluid project-image rounded"
-                  height={100}
+                  height={110}
                 />
               </a>
               <div className="col-md-5 px-3">
-                <div className="primary-text-color fw-bold my-3">#REACT</div>
-                <h5 className="fw-bold">Obscura Media Limited</h5>
+                <div className="primary-text-color fw-bold my-3">
+                  REACT and TYPESCRIPT
+                </div>
+                <h5 className="fw-bold">PowWater Website</h5>
                 <p>
-                  Obscura Media Limited is a Kenyan based communications firm
-                  with decades of global experience. Obsura specialize in all
-                  aspects of media, communications, and public relations
-                  including film, photography, design, printing, and campaign
-                  and communications consulting.
+                  I initiated and completed the development of PowWater's
+                  website from scratch. Using React.js and TypeScript, I ensured
+                  a seamless user experience and robust codebase. This project,
+                  my first at the company, showcased my proficiency in modern
+                  web development technologies
                 </p>
                 <div className="my-2">
-                  <i className="fab fa-react fa-lg text-info"></i>
+                  <i className="fab fa-react fa-lg text-info me-2"></i>
+                  <img src={ts} alt="TS" width="20" />
                 </div>
                 <a
                   className="fw-bold text-uppercase"
-                  href="https://master--brilliant-hotteok-f20887.netlify.app/"
+                  href="https://powwater.com"
                   target="_blank"
                   rel="noreferrer"
                 >
@@ -58,41 +63,39 @@ const Projects = ({ projectsDivRef }) => {
 
           <Fade delay={540} bottom>
             <div className="d-flex justify-content-between flex-wrap project-card my-5 p-3 bg-white rounded shadow-sm">
-              <a
-                className="bg-pink p-3 col-md-6 image-card"
-                href="https://master--statuesque-semolina-0016f3.netlify.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
+              <div className="shadow rounded p-3 col-md-6 image-card">
                 <img
                   alt="project"
-                  src={environmental}
+                  src={dashboad}
                   className="img-fluid project-image rounded"
                   height={100}
                 />
-              </a>
+              </div>
               <div className="col-md-5 px-3">
-                <div className="primary-text-color fw-bold my-3">#REACT</div>
-                <h5 className="fw-bold text-capitalize">
-                  open institute environmental club
-                </h5>
-                <p>
-                  Environmental Club offers events and activities of all kinds
-                  for students interested in breading their horizons, creating
-                  amazing memories and learning more about environment.
-                </p>
-                <div className="my-2">
-                  <i className="fab fa-react fa-lg text-info"></i>
+                <div className="primary-text-color fw-bold my-3">
+                  REACT, TYPESCRIPT and NODE
                 </div>
-                <a
-                  className="fw-bold text-uppercase"
-                  href="https://master--statuesque-semolina-0016f3.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i class="fas fa-external-link"></i>
-                  <span className="mx-3">View live Website</span>
-                </a>
+                <h5 className="fw-bold text-capitalize">PowWater Dashboard</h5>
+                <p>
+                  For PowWater's internal operations, I led the development of a
+                  comprehensive dashboard utilizing React.js and TypeScript for
+                  the frontend and Node.js for creating APIs. This dashboard
+                  serves as a centralized hub for report generation, order
+                  management, and assignment. An integral aspect of this project
+                  was integrating the dashboard with QuickBooks for seamless
+                  invoicing and statement management. Additionally, I
+                  implemented integration with M-Pesa for efficient payment
+                  processing, enhancing the dashboard's functionality and
+                  utility for PowWater's operations.
+                </p>
+                <div className="my-2 d-flex justify-content-between col-md-3 align-items-center">
+                  <i className="fab fa-react fa-lg text-info"></i>
+                  <i className="fab fa-node fa-2x"></i>
+                  <img src={ts} alt="TS" width="20" />
+                </div>
+                <div className="fw-bold text-uppercase">
+                  <span className="mx-3">Request Demo</span>
+                </div>
               </div>
             </div>
           </Fade>
@@ -100,7 +103,7 @@ const Projects = ({ projectsDivRef }) => {
           <Fade delay={560} bottom>
             <div className="d-flex justify-content-between flex-wrap project-card my-5 p-3 bg-white rounded shadow-sm">
               <a
-                className="bg-secondary p-3 col-md-6 image-card"
+                className="shadow rounded p-3 col-md-6 image-card"
                 href="https://play.google.com/store/apps/details?id=com.powwater.customer&hl=en&gl=US"
                 target="_blank"
                 rel="noreferrer"
@@ -114,15 +117,21 @@ const Projects = ({ projectsDivRef }) => {
               </a>
               <div className="col-md-5 px-3">
                 <div className="primary-text-color fw-bold my-3">
-                  #REACT NATIVE
+                  REACT NATIVE, TYPESCRIPT and NODE
                 </div>
                 <h5 className="fw-bold text-capitalize">PowWater Mobile App</h5>
                 <p>
-                  Powwater mobile app helps people in Kenya to order bowser
-                  water at the comfort of their homes.
+                  I developed the PowWater mobile app using React Native for the
+                  frontend and Node.js for API development. The app enables
+                  customers to conveniently place orders, make payments, and
+                  access their ordering patterns. This project streamlined the
+                  customer experience, enhancing PowWater's service
+                  accessibility and efficiency.
                 </p>
-                <div className="my-2">
+                <div className="my-2 d-flex justify-content-between col-md-3 align-items-center">
                   <i className="fab fa-react fa-lg text-info"></i>
+                  <i className="fab fa-node fa-2x"></i>
+                  <img src={ts} alt="TS" width="20" />
                 </div>
                 <a
                   className="fw-bold text-uppercase"
@@ -133,6 +142,43 @@ const Projects = ({ projectsDivRef }) => {
                   <i class="fas fa-external-link"></i>
                   <span className="mx-3">Download The App</span>
                 </a>
+              </div>
+            </div>
+          </Fade>
+
+          <Fade delay={580} bottom>
+            <div className="d-flex justify-content-between flex-wrap project-card my-5 p-3 bg-white rounded shadow-sm">
+              <div className="shadow rounded p-3 col-md-6 image-card">
+                <img
+                  alt="project"
+                  src={partnership}
+                  className="img-fluid project-image rounded"
+                  height={100}
+                />
+              </div>
+              <div className="col-md-5 px-3">
+                <div className="primary-text-color fw-bold my-3">
+                  REACT, TYPESCRIPT and NODE
+                </div>
+                <h5 className="fw-bold text-capitalize">
+                  PowWater Partnership Portal
+                </h5>
+                <p>
+                  I designed and implemented a partners portal for PowWater,
+                  enabling third-party individuals to track assigned orders,
+                  view earnings, and request payments. Leveraging React for the
+                  frontend, and Node.js with TypeScript for backend development,
+                  this portal optimizes efficiency and transparency in
+                  PowWater's partnership ecosystem.
+                </p>
+                <div className="my-2 d-flex justify-content-between col-md-3 align-items-center">
+                  <i className="fab fa-react fa-lg text-info"></i>
+                  <i className="fab fa-node fa-2x"></i>
+                  <img src={ts} alt="TS" width="20" />
+                </div>
+                <div className="fw-bold text-uppercase">
+                  <span className="mx-3">Request Demo</span>
+                </div>
               </div>
             </div>
           </Fade>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
 import ts from "../images/Typescript_logo.png";
+import google_cloud from "../images/google_cloud.png";
+import sql from "../images/sql.jpg";
+import qb from "../images/qb.jpg";
+import mpesa from "../images/mpesa.png";
 
 const Skills = ({ skillsDivRef }) => {
   return (
@@ -19,18 +23,28 @@ const Skills = ({ skillsDivRef }) => {
               </div>
               <div className="about-info">
                 <p>
-                  I have a deep passion for my work, fueled by the desire to
-                  solve problems and create value. I am a self-motivated and
-                  self-driven individual who is always looking for new
-                  challenges and opportunities to grow and improve
+                  I am a seasoned software developer proficient in a versatile
+                  tech stack, including{" "}
+                  <span className="yellow-text fw-bold">
+                    React.js, Node.js, TypeScript, and React Native
+                  </span>
+                  . With a passion for innovation and a commitment to
+                  excellence, I specialize in crafting dynamic solutions that
+                  elevate user experiences across web and mobile platforms. My
+                  expertise spans the development of robust web applications,
+                  mobile apps, and backend systems.
                 </p>
                 <p>
-                  My experience and expertise as a software engineer is
-                  multi-faceted with a keen focus on front-end web development
-                  with <span className="yellow-text fw-bold">React</span>. I've
-                  also worked on a variety of cross-platform mobile apps built
-                  with <span className="yellow-text fw-bold">React Native</span>
-                  .
+                  I have a proven track record of delivering high-quality
+                  solutions tailored to meet diverse project requirements.
+                  Additionally, I excel in creating intuitive user interfaces,
+                  optimizing performance, and integrating third-party services
+                  to enhance functionality. My skill set enables me to tackle
+                  complex challenges and drive projects from conception to
+                  completion with precision and efficiency. Driven by a
+                  relentless pursuit of mastery in software development, I
+                  continually explore emerging technologies and best practices
+                  to stay at the forefront of the industry.
                 </p>
               </div>
             </div>
@@ -63,6 +77,28 @@ const Skills = ({ skillsDivRef }) => {
                   <div className="text-uppercase bg-white shadow rounded p-4 col-md-5 mb-3 d-flex align-items-center skill-card">
                     <i className="fab fa-js-square fa-2x"></i>
                     <span className="ps-3 fw-bold skill-size">JAVASCRIPT</span>
+                  </div>
+                  <div className="text-uppercase bg-white shadow rounded p-4 col-md-5 mb-3 d-flex align-items-center skill-card">
+                    <img src={google_cloud} alt="TS" width="30" />
+                    <span className="ps-3 fw-bold skill-size">
+                      GOOGLE CLOUD PLATFORM
+                    </span>
+                  </div>
+                  <div className="text-uppercase bg-white shadow rounded p-4 col-md-5 mb-3 d-flex align-items-center skill-card">
+                    <img src={sql} alt="TS" width="30" />
+                    <span className="ps-3 fw-bold skill-size">SQL</span>
+                  </div>
+                  <div className="text-uppercase bg-white shadow rounded p-4 col-md-5 mb-3 d-flex align-items-center skill-card">
+                    <img src={qb} alt="TS" width="30" />
+                    <span className="ps-3 fw-bold skill-size">
+                      QUICKBOOKS INTEGRATION
+                    </span>
+                  </div>
+                  <div className="text-uppercase bg-white shadow rounded p-4 col-md-5 mb-3 d-flex align-items-center skill-card">
+                    <img src={mpesa} alt="TS" width="40" />
+                    <span className="ps-3 fw-bold skill-size">
+                      M-PESA INTEGRATION
+                    </span>
                   </div>
                 </div>
               </div>
